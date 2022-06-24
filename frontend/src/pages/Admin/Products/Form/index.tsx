19692a265f2f7a166aba994d52985084359bab2f
Lucas Gevaerd <lucaasgevaerd@gmail.com>
2022-06-24 16:16:53 -0300

    Retrieving categories from back end

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,8 +1,9 @@
 import { AxiosRequestConfig } from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useHistory, useParams } from 'react-router-dom';
 import Select from 'react-select';
+import { Category } from 'types/category';
 import { Product } from 'types/product';
 import { requestBackend } from 'util/requests';
 
@@ -14,17 +15,13 @@ type Urlparams = {
 
 const Form = () => {
 
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' }
-  ];
-
   const { productId } = useParams<Urlparams>();
 
   const isEditing = productId !== 'create';
 
   const history = useHistory();
+
+  const [selectCategories, setSelectCategories] = useState<Category[]>([]);
 
   const {
     register,
@@ -32,6 +29,13 @@ const Form = () => {
     formState: { errors },
     setValue,
   } = useForm<Product>();
+
+  useEffect(() => {
+    requestBackend({url: '/categories'})
+      .then(response => {
+        setSelectCategories(response.data.content);
+      })
+  }, []);
 
   useEffect(() => {
     if (isEditing) {
@@ -98,9 +102,11 @@ const Form = () => {
 
               <div className="margin-bottom-30">
                 <Select
-                  options={options}
+                  options={selectCategories}
                   classNamePrefix="product-crud-select"
                   isMulti
+                  getOptionLabel={(category: Category) => category.name}
+                  getOptionValue={(category: Category) => String(category.id)}
                 />                 
               </div>
 
