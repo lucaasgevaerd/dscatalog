c0e06e9bc5b76012495659a6b1ea213c6b8aa526
Lucas Gevaerd <lucaasgevaerd@gmail.com>
2022-06-28 14:48:54 -0300

    Toast

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -7,6 +7,7 @@ import Select from 'react-select';
 import { Category } from 'types/category';
 import { Product } from 'types/product';
 import { requestBackend } from 'util/requests';
+import { toast } from 'react-toastify';
 
 import './styles.css';
 
@@ -65,7 +66,11 @@ const Form = () => {
     };
 
     requestBackend(config).then(() => {
+      toast.info('Produto cadastrado com sucesso');
       history.push('/admin/products');
+    })
+    .catch(() => {
+      toast.error('Erro ao cadastrar produto');
     });
   };
 
