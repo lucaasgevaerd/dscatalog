9ba9de4f4937b3234d7ed8fb0a61970ccdb738b0
Lucas Gevaerd <lucaasgevaerd@gmail.com>
2022-08-08 18:57:50 -0300

    REFACTOR token, Role

--- frontend/src/util/__tests__/formatters.spec.ts
+++ frontend/src/util/__tests__/formatters.spec.ts
@@ -1,14 +1,33 @@
 import { FormatPrice } from "util/formatters";
 
-test('formatPrice should format number pt-BR when given 10.1', () => {
+describe('formatPrice for positive numbers', () => {
+  //it('formatPrice should format number pt-BR when given 10.1', () => {
+    test('formatPrice should format number pt-BR when given 10.1', () => {
+        // ARRANGE
+        const value = 10.1;
+        // ACT
+        const result = FormatPrice(value);
+        // ASSERT
+        expect(result).toEqual('10,10');
+    });
 
-    // ARRANGE
-    const value = 10.1;
+    test('formatPrice should format number pt-BR when given 0.1', () => {
+        const value = 0.1;
+        const result = FormatPrice(value);
+        expect(result).toEqual('0,10');
+    });
+})
 
-    // ACT
-    const result = FormatPrice(value);
+describe('formatPrice for non-positive numbers', () => {
+    test('formatPrice should format number pt-BR when given 0', () => {
+        const value = 0;
+        const result = FormatPrice(value);
+        expect(result).toEqual('0,00');
+    });
 
-    // ASSERT
-    expect(result).toEqual('10,10');
-
-});
+    test('formatPrice should format number pt-BR when given -5.1', () => {
+        const value = -5.1;
+        const result = FormatPrice(value);
+        expect(result).toEqual('-5,10');
+    });
+})
