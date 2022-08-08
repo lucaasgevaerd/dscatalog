fcb0e7763835f24cfbeff1cb594e265d40a9aefd
Lucas Gevaerd <lucaasgevaerd@gmail.com>
2022-08-08 20:32:13 -0300

    Mocking function with spyOn

--- frontend/src/util/__tests__/formatters.spec.ts
+++ frontend/src/util/__tests__/formatters.spec.ts
@@ -10,12 +10,6 @@ describe('formatPrice for positive numbers', () => {
         // ASSERT
         expect(result).toEqual('10,10');
     });
-
-    test('formatPrice should format number pt-BR when given 0.1', () => {
-        const value = 0.1;
-        const result = FormatPrice(value);
-        expect(result).toEqual('0,10');
-    });
 })
 
 describe('formatPrice for non-positive numbers', () => {
@@ -23,11 +17,5 @@ describe('formatPrice for non-positive numbers', () => {
         const value = 0;
         const result = FormatPrice(value);
         expect(result).toEqual('0,00');
-    });
-
-    test('formatPrice should format number pt-BR when given -5.1', () => {
-        const value = -5.1;
-        const result = FormatPrice(value);
-        expect(result).toEqual('-5,10');
     });
 })
