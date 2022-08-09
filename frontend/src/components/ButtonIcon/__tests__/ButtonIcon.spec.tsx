767a92593fe691308335c7dd3e34265b30b5c7c5
Lucas Gevaerd <lucaasgevaerd@gmail.com>
2022-08-09 12:37:03 -0300

    ProductPrice, ProductCard tests

--- frontend/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
+++ frontend/src/components/ButtonIcon/__tests__/ButtonIcon.spec.tsx
@@ -8,7 +8,6 @@ test('ButtonIcon should render button with given text', () => {
     render(
         <ButtonIcon text={text} />
     );
-    screen.debug();
     // ASSERT
     expect(screen.getByText(text)).toBeInTheDocument();
 })
